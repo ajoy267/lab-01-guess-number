aa599dc67b2241f74fa317456976d4f492a231e1
ajoy267 <andrew.joy12@hotmail.com>
2021-10-04 16:08:35 -0700

    fixed eslint issues with pushing code

--- app.js
+++ app.js
@@ -3,28 +3,27 @@ const userInput = document.getElementById('user-input');
 
 //buttons
 const button = document.getElementById('guess-button');
-const reset = document.getElementById('play-again');
 
 //Results
 const resultMessage = document.getElementById('result-message');
 
 //numbers
-let numGuessedRemaining = document.getElementById('number-of-guesses')
-const randomNum = Math.floor(Math.random()*20)+1;
+let numGuessedRemaining = document.getElementById('number-of-guesses');
+const randomNum = Math.floor(Math.random() * 20) + 1;
 let numGuessed = 4;
 
 button.addEventListener('click', () => {
     const userGuess = Number(userInput.value);
     numGuessed--;
     numGuessedRemaining.textContent = numGuessed;
-    let result
+    let result;
 
     if (userGuess === randomNum) {
         result = `You're Correct, You Win!`;
     } else if (userGuess < randomNum) {
-      result = `Guess Too Low, Try Again`;
+        result = `Guess Too Low, Try Again`;
     } else {
-      result = `Guess Too High, Try Again!`;
+        result = `Guess Too High, Try Again!`;
     }
     resultMessage.textContent = result;
 });
