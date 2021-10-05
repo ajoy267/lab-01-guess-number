200347eaadb8adc4a832a036ed86f4308d04cb19
ajoy267 <andrew.joy12@hotmail.com>
2021-10-04 17:04:13 -0700

    made the reset button to work and reset the page

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ const userInput = document.getElementById('user-input');
 
 //buttons
 const button = document.getElementById('guess-button');
+const resetButton = document.getElementById('play-again');
 
 //Results
-const resultMessage = document.getElementById('result-message');
+let resultMessage = document.getElementById('result-message');
 
 //numbers
 let numGuessedRemaining = document.getElementById('number-of-guesses');
-const randomNum = Math.floor(Math.random() * 20) + 1;
+let randomNum = Math.floor(Math.random() * 20) + 1;
 let numGuessed = 4;
 
 button.addEventListener('click', () => {
@@ -20,16 +21,15 @@ button.addEventListener('click', () => {
 
     if (userGuess === randomNum) {
         result = `You're Correct, You Win!`;
-        button.disabled = true;
     } else if (userGuess < randomNum) {
         result = `Guess Too Low, Try Again`;
-        button.disabled = false;
     } else {
         result = `Guess Too High, Try Again!`;
-        button.disabled = false;
     }
     if (numGuessed === 0) {
         result = `You ran out of guesses! Game Over! The answer was ${randomNum}`;
+        button.disabled = true;
+    } else if (userGuess === randomNum) {
         button.disabled = true;
     } else {
         button.disabled = false;
@@ -37,3 +37,11 @@ button.addEventListener('click', () => {
     resultMessage.textContent = result;
 });
 
+resetButton.addEventListener('click', () => {
+    let result;
+    resultMessage.textContent = result;
+    numGuessed = 4;
+    button.disabled = false;
+    numGuessedRemaining.textContent = numGuessed;
+    randomNum = Math.floor(Math.random() * 20) + 1;
+});
